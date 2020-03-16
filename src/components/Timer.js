dd471218a226155f76298f95b6a192adb9115e6f
Senthil Sivaramakrishnanan <senthil.sivaramakrishnan.ext@proximus.com@Senthils-MacBook-Pro-Px.local>
2020-03-16 23:10:07 +0100

    UPDATE style

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 
 export const Timer = ({ seconds }) => (
-  <div className="marginTopTwenty">
+  <div className="marginTopTwenty alignCenter">
     <Typography
       variant="subtitle1"
-      className={`alignCenter ${seconds < 10 ? 'redColor' : 'default'}`}
+      className={`${seconds < 10 ? 'redColor' : 'default'}`}
     >
       Time Remaining:
       <span className="boldText">
